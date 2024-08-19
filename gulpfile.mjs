dba8d5fecec74852364dc69b2d75b8ac96894b55
Palady56 <paladyultra@gmail.com>
2024-08-19 16:00:13 +0300

    initial commit

---
+++ gulpfile.mjs
@@ -0,0 +1,42 @@
+import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
+import dartSass from 'sass';
+import autoprefixer from 'gulp-autoprefixer';
+import cleanCSS from 'gulp-clean-css';
+import browserSync from 'browser-sync';
+import sourcemaps from 'gulp-sourcemaps';
+
+const sass = gulpSass(dartSass);
+
+gulp.task('styles', function() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+// reload
+gulp.task('serve', function() {
+    browserSync.init({
+        server: './dist'
+    });
+
+    gulp.watch('src/scss/**/*.scss', gulp.series('styles'));
+    gulp.watch('src/*.html').on('change', gulp.series('copy-html', browserSync.reload));
+});
+
+// html
+gulp.task('copy-html', function() {
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', gulp.series('copy-html', 'styles', 'serve'));
+
+
